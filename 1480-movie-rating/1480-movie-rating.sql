57864c449e9cd6b6a977caf5c4cf760fd143568e
Viranchi More <159642625+viru9192@users.noreply.github.com>
2025-07-18 17:29:50 -0400

    Time: 1273 ms (43.52%), Space: 0B (100%) - LeetHub

---
+++ 1480-movie-rating/1480-movie-rating.sql
@@ -0,0 +1,22 @@
+select results from (
+select u.name as results
+from users u
+join movierating mr
+on u.user_id = mr.user_id
+group by u.user_id, u.name
+order by count(*) desc, u.name asc
+limit 1
+) as top_user
+
+union all
+
+select results from (
+select m.title as results
+from movies m
+join movierating mr
+on m.movie_id = mr.movie_id
+where year(mr.created_at) = 2020 and month(mr.created_at) = 2
+group by m.movie_id, m.title
+order by avg(mr.rating) desc, m.title asc
+limit 1
+) as top_movie;
