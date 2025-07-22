343aed9f626dc3e068648d36abe0753404cf2adc
Viranchi More <159642625+viru9192@users.noreply.github.com>
2025-07-22 09:50:59 -0400

    Time: 741 ms (21.73%), Space: 0B (100%) - LeetHub

--- 1292-immediate-food-delivery-ii/1292-immediate-food-delivery-ii.sql
+++ 1292-immediate-food-delivery-ii/1292-immediate-food-delivery-ii.sql
@@ -1,6 +1,6 @@
 select (
     round(
-        sum(case when order_date = customer_pref_delivery_date then 1 else 0 end) * 100.0 / count(*), 2 )
+        sum(case when order_date = customer_pref_delivery_date then 1 else 0 end) * 100.0 / count(*), 2)
 ) as immediate_percentage
 from (
     select d.*
