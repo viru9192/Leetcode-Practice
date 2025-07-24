8aa9260caaf2b1836ab702519c18abd31d9ab686
Viranchi More <159642625+viru9192@users.noreply.github.com>
2025-07-23 23:09:56 -0400

    Time: 367 ms (25.73%), Space: 0B (100%) - LeetHub

--- 2127-employees-whose-manager-left-the-company/2127-employees-whose-manager-left-the-company.sql
+++ 2127-employees-whose-manager-left-the-company/2127-employees-whose-manager-left-the-company.sql
@@ -1,7 +1,8 @@
-select
-    employee_id
-from Employees
-where manager_id is not null and manager_id not in (
-    select employee_id from Employees
-    ) and salary < 30000
-order by 1
+select employee_id
+from employees
+where manager_id is not null
+and 
+manager_id not in (
+    select employee_id from employees
+) and salary < 30000
+order by employee_id;
