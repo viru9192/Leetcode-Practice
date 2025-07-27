9e2f77e75d001c9a0ab33e62d7933d15cdd20c9c
Viranchi More <159642625+viru9192@users.noreply.github.com>
2025-07-27 12:25:19 -0400

    Time: 398 ms (28.86%), Space: 0B (100%) - LeetHub

--- 3803-find-products-with-valid-serial-numbers/3803-find-products-with-valid-serial-numbers.sql
+++ 3803-find-products-with-valid-serial-numbers/3803-find-products-with-valid-serial-numbers.sql
@@ -1,4 +1,4 @@
-SELECT *
-FROM products
-WHERE REGEXP_LIKE(description, '(^|[^A-Za-z0-9])SN[0-9]{4}-[0-9]{4}([^0-9]|$)', 'c')
-ORDER BY product_id;
+select *
+from products
+where regexp_like(description, '(^|[^A-Za-z0-9])SN[0-9]{4}-[0-9]{4}([^0-9]|$)', 'c')
+order by product_id;
