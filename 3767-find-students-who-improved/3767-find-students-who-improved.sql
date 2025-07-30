33c28429a3bfcbcde2ee9230325b1d052e6a1449
Viranchi More <159642625+viru9192@users.noreply.github.com>
2025-07-29 21:05:38 -0400

    Time: 293 ms (57.32%), Space: 0B (100%) - LeetHub

--- 3767-find-students-who-improved/3767-find-students-who-improved.sql
+++ 3767-find-students-who-improved/3767-find-students-who-improved.sql
@@ -1,17 +1,13 @@
-SELECT DISTINCT student_id, subject, first_score, latest_score
-FROM (
-    SELECT 
-        student_id,
-        subject,
-        FIRST_VALUE(score) OVER (
-            PARTITION BY student_id, subject 
-            ORDER BY exam_date
-        ) AS first_score,
-        FIRST_VALUE(score) OVER (
-            PARTITION BY student_id, subject 
-            ORDER BY exam_date DESC
-        ) AS latest_score
-    FROM Scores
+select distinct student_id, subject, first_score, latest_score
+from (
+    select 
+    student_id,
+    subject,
+    first_value(score) over (partition by student_id, subject 
+    order by exam_date) as first_score,
+    first_value(score) over (partition by student_id, subject
+    order by exam_date desc) as latest_score
+    from scores
 ) t
-WHERE latest_score > first_score
-ORDER BY student_id, subject;
+where latest_score > first_score
+order by student_id, subject;
