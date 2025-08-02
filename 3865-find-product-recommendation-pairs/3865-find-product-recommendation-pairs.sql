1b76ddeb53ea13104d0dd7d020ed3aa497a5bbfa
Viranchi More <159642625+viru9192@users.noreply.github.com>
2025-08-02 13:22:59 -0400

    Time: 1235 ms (5.2%), Space: 0B (100%) - LeetHub

--- 3865-find-product-recommendation-pairs/3865-find-product-recommendation-pairs.sql
+++ 3865-find-product-recommendation-pairs/3865-find-product-recommendation-pairs.sql
@@ -1,19 +1,17 @@
-SELECT 
-    p1.product_id AS product1_id,
-    p2.product_id AS product2_id,
-    pi1.category AS product1_category,
-    pi2.category AS product2_category,
-    COUNT(DISTINCT p1.user_id) AS customer_count
-FROM ProductPurchases p1
-JOIN ProductPurchases p2
-    ON p1.user_id = p2.user_id
-   AND p1.product_id < p2.product_id
-JOIN ProductInfo pi1
-    ON p1.product_id = pi1.product_id
-JOIN ProductInfo pi2
-    ON p2.product_id = pi2.product_id
-GROUP BY p1.product_id, p2.product_id, pi1.category, pi2.category
-HAVING COUNT(DISTINCT p1.user_id) >= 3
-ORDER BY customer_count DESC, 
-         product1_id ASC, 
-         product2_id ASC;
+select 
+p1.product_id as product1_id,
+p2.product_id as product2_id,
+pi1.category as product1_category,
+pi2.category as product2_category,
+count(distinct p1.user_id) as customer_count
+from ProductPurchases p1
+join ProductPurchases p2
+on p1.user_id = p2.user_id 
+and p1.product_id < p2.product_id
+join ProductInfo pi1 
+on p1.product_id = pi1.product_id
+join ProductInfo pi2
+on p2.product_id = pi2.product_id
+group by p1.product_id, p2.product_id, pi1.category, pi2.category
+having count(distinct p1.user_id) >= 3
+order by customer_count desc, product1_id asc, product2_id asc;
