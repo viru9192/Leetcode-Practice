1934e30d9142f380f436796338bf6704ec3360c4
Viranchi More <159642625+viru9192@users.noreply.github.com>
2025-08-03 21:41:09 -0400

    Time: 429 ms (99.95%), Space: 0B (100%) - LeetHub

--- 3921-find-consistently-improving-employees/3921-find-consistently-improving-employees.sql
+++ 3921-find-consistently-improving-employees/3921-find-consistently-improving-employees.sql
@@ -1,33 +1,33 @@
-WITH ranked_reviews AS (
-    SELECT 
-        pr.employee_id,
-        pr.review_date,
-        pr.rating,
-        ROW_NUMBER() OVER (PARTITION BY pr.employee_id ORDER BY pr.review_date DESC) AS rn
-    FROM performance_reviews pr
+with r_reviews as (
+    select 
+    pr.employee_id,
+    pr.rating,
+    pr.review_date,
+    row_number() over (partition by pr.employee_id order by pr.review_date desc) as rn
+    from performance_reviews pr
 ),
-last_three AS (
-    SELECT 
-        employee_id,
-        MAX(CASE WHEN rn = 1 THEN rating END) AS r1,  -- most recent
-        MAX(CASE WHEN rn = 2 THEN rating END) AS r2,
-        MAX(CASE WHEN rn = 3 THEN rating END) AS r3
-    FROM ranked_reviews
-    WHERE rn <= 3
-    GROUP BY employee_id
+last_three as (
+    select employee_id,
+    max(case when rn = 1 then rating end) as r1,
+    max(case when rn = 2 then rating end) as r2,
+    max(case when rn = 3 then rating end) as r3
+    from r_reviews 
+    where rn <= 3
+    group by employee_id
 ),
-qualified AS (
-    SELECT 
-        e.employee_id,
-        e.name,
-        (r1 - r3) AS improvement_score
-    FROM last_three lt
-    JOIN employees e ON e.employee_id = lt.employee_id
-    WHERE r1 > r2 AND r2 > r3   -- strictly increasing
+qualified as (
+    select 
+    e.employee_id,
+    e.name,
+    (r1 - r3) as improvement_score
+    from last_three lt
+    join employees e
+    on lt.employee_id = e.employee_id
+    where (r1 >r2) and (r2 > r3)
 )
-SELECT 
-    employee_id,
-    name,
-    improvement_score
-FROM qualified
-ORDER BY improvement_score DESC, name ASC;
+select 
+employee_id,
+name,
+improvement_score
+from qualified
+order by improvement_score desc, name asc;
