fe1f3dc2b7aacd9481d827cb6eeee3b827bf95ea
Viranchi More <159642625+viru9192@users.noreply.github.com>
2025-08-07 22:03:15 -0400

    Time: 690 ms (14.33%), Space: 0B (100%) - LeetHub

--- 3971-find-stores-with-inventory-imbalance/3971-find-stores-with-inventory-imbalance.sql
+++ 3971-find-stores-with-inventory-imbalance/3971-find-stores-with-inventory-imbalance.sql
@@ -1,53 +1,53 @@
-WITH store_prod_counts AS (
-  SELECT
+with store_prod_counts as (
+  select
     i.store_id,
-    COUNT(DISTINCT i.product_name) AS product_cnt
-  FROM inventory i
-  GROUP BY i.store_id
+    count(distinct i.product_name) as product_cnt
+  from inventory i
+  group by i.store_id
 ),
-ranked AS (
-  SELECT
+ranked as (
+  select
     i.store_id,
     i.product_name,
     i.quantity,
     i.price,
-    ROW_NUMBER() OVER (
-      PARTITION BY i.store_id
-      ORDER BY i.price DESC, i.product_name ASC
-    ) AS rn_max_price,
-    ROW_NUMBER() OVER (
-      PARTITION BY i.store_id
-      ORDER BY i.price ASC, i.product_name ASC
-    ) AS rn_min_price
-  FROM inventory i
+    row_number() over (
+      partition by i.store_id
+      order by i.price desc, i.product_name asc
+    ) as rn_max_price,
+    row_number() over (
+      partition by i.store_id
+      order by i.price asc, i.product_name asc
+    ) as rn_min_price
+  from inventory i
 ),
-most_exp AS (
-  SELECT
+most_exp as (
+  select
     store_id,
-    product_name AS most_exp_product,
-    quantity    AS most_exp_qty
-  FROM ranked
-  WHERE rn_max_price = 1
+    product_name as most_exp_product,
+    quantity    as most_exp_qty
+  from ranked
+  where rn_max_price = 1
 ),
-cheapest AS (
-  SELECT
+cheapest as (
+  select
     store_id,
-    product_name AS cheapest_product,
-    quantity    AS cheapest_qty
-  FROM ranked
-  WHERE rn_min_price = 1
+    product_name as cheapest_product,
+    quantity    as cheapest_qty
+  from ranked
+  where rn_min_price = 1
 )
-SELECT
+select
   s.store_id,
   s.store_name,
   s.location,
   me.most_exp_product,
   ch.cheapest_product,
-  ROUND( ch.cheapest_qty * 1.0 / me.most_exp_qty, 2 ) AS imbalance_ratio
-FROM stores s
-JOIN store_prod_counts c ON c.store_id = s.store_id AND c.product_cnt >= 3
-JOIN most_exp me         ON me.store_id = s.store_id
-JOIN cheapest ch         ON ch.store_id = s.store_id
-WHERE me.most_exp_qty < ch.cheapest_qty         -- imbalance condition
-  AND me.most_exp_qty > 0                       -- (optional) avoid divide-by-zero
-ORDER BY imbalance_ratio DESC, s.store_name ASC;
+  round(ch.cheapest_qty * 1.0 / me.most_exp_qty, 2) as imbalance_ratio
+from stores s
+join store_prod_counts c on c.store_id = s.store_id and c.product_cnt >= 3
+join most_exp me         on me.store_id = s.store_id
+join cheapest ch         on ch.store_id = s.store_id
+where me.most_exp_qty < ch.cheapest_qty
+  and me.most_exp_qty > 0
+order by imbalance_ratio desc, s.store_name asc;
