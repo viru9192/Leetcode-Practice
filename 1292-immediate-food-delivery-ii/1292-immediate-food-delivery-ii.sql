0896565608cece20a5d69c5680f412adc5ba0ed8
Viranchi More <159642625+viru9192@users.noreply.github.com>
2025-08-16 12:59:33 -0400

    Time: 608 ms (67.63%), Space: 0B (100%) - LeetHub

--- 1292-immediate-food-delivery-ii/1292-immediate-food-delivery-ii.sql
+++ 1292-immediate-food-delivery-ii/1292-immediate-food-delivery-ii.sql
@@ -1,16 +1,18 @@
-select (
-    round(
-        sum(case when order_date = customer_pref_delivery_date then 1 else 0 end) * 100.0 / count(*), 2)
+select 
+round(
+    sum(case when order_date = customer_pref_delivery_date then 1 else 0 end) * 100 /
+    count(*), 2
 ) as immediate_percentage
 from (
     select d.*
     from delivery d
     join (
         select customer_id, min(order_date) as first_order_date
-        from delivery
+        from delivery 
         group by customer_id
-    ) as first_order
-    on d.customer_id = first_order.customer_id
-    and
-    d.order_date = first_order.first_order_date
+    ) as first_customer_order
+on
+d.customer_id = first_customer_order.customer_id
+and 
+d.order_date = first_customer_order.first_order_date 
 ) as cf
