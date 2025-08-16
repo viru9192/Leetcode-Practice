e766eb98a46960030c665a2fc42e2291edda487a
Viranchi More <159642625+viru9192@users.noreply.github.com>
2025-08-16 12:00:20 -0400

    Time: 1397 ms (42.97%), Space: 0B (100%) - LeetHub

--- 1724-customer-who-visited-but-did-not-make-any-transactions/1724-customer-who-visited-but-did-not-make-any-transactions.sql
+++ 1724-customer-who-visited-but-did-not-make-any-transactions/1724-customer-who-visited-but-did-not-make-any-transactions.sql
@@ -1,4 +1,6 @@
-select distinct v.customer_id, count(*) as count_no_trans
+select 
+distinct v.customer_id,
+count(*) as count_no_trans
 from visits v
 left join transactions t
 on v.visit_id = t.visit_id
