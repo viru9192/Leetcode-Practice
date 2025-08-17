2003f9444171283174b5055d30bd8c04fec709c2
Viranchi More <159642625+viru9192@users.noreply.github.com>
2025-08-17 13:25:14 -0400

    Time: 838 ms (37.41%), Space: 0B (100%) - LeetHub

--- 1390-average-selling-price/1390-average-selling-price.sql
+++ 1390-average-selling-price/1390-average-selling-price.sql
@@ -1,10 +1,14 @@
-select p.product_id, round(
-    ifnull(sum(u.units * p.price) / sum(u.units), 0), 2
+select 
+p.product_id,
+round(
+    ifnull(
+        sum(u.units * p.price) / sum(u.units) 
+    , 0), 2
 ) as average_price
 from prices p
-left join unitsSold u
-on 
-p.product_id = u.product_id
-and 
-u.purchase_date between p.start_date and p.end_date
-group by product_id;
+left join unitssold u
+on p.product_id = u.product_id
+and (
+    u.purchase_date between p.start_date and p.end_date 
+)
+group by p.product_id;
