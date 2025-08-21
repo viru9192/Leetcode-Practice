5e20a50c4ee63e766b0f27d903e89f16ee177c06
Viranchi More <159642625+viru9192@users.noreply.github.com>
2025-08-20 20:11:08 -0400

    Time: 1423 ms (7.68%), Space: 0B (100%) - LeetHub

--- 1278-product-price-at-a-given-date/1278-product-price-at-a-given-date.sql
+++ 1278-product-price-at-a-given-date/1278-product-price-at-a-given-date.sql
@@ -1,10 +1,13 @@
-select distinct p.product_id,
-    coalesce((
+select 
+distinct p.product_id,
+coalesce (
+    (
         select new_price
-        from products 
-        where product_id = p.product_id 
+        from products
+        where product_id = p.product_id
         and change_date <= '2019-08-16'
         order by change_date desc
         limit 1
-    ),10) as price
+    ), 10
+) as price
 from products p;
