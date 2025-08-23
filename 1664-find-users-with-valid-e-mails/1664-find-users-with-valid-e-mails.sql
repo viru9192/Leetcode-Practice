d3fac89b14e1b1fce27033067080ffc6ab9ab29b
Viranchi More <159642625+viru9192@users.noreply.github.com>
2025-08-23 12:23:18 -0400

    Time: 732 ms (44.2%), Space: 0B (100%) - LeetHub

--- 1664-find-users-with-valid-e-mails/1664-find-users-with-valid-e-mails.sql
+++ 1664-find-users-with-valid-e-mails/1664-find-users-with-valid-e-mails.sql
@@ -1,5 +1,9 @@
 select *
-from users
-where
-mail regexp '^[A-Za-z][A-Za-z0-9._-]*@leetcode\\.com$'
-and binary mail like '%@leetcode.com';
+from 
+users
+where 
+mail regexp(
+    '^[A-Za-z][A-Za-z0-9._-]*@leetcode\\.com$'
+)
+and
+binary mail like '%leetcode.com';
