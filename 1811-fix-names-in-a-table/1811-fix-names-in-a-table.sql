609c396eecf0aac5214a606518ec57618cc3c6ff
Viranchi More <159642625+viru9192@users.noreply.github.com>
2025-08-23 11:35:36 -0400

    Time: 634 ms (53.85%), Space: 0B (100%) - LeetHub

--- 1811-fix-names-in-a-table/1811-fix-names-in-a-table.sql
+++ 1811-fix-names-in-a-table/1811-fix-names-in-a-table.sql
@@ -1,4 +1,13 @@
-select user_id,
-    concat(upper(left(name, 1)), lower(substring(name, 2, length(name)))) as name
+select 
+user_id,
+concat(
+    upper(
+        left(name, 1)
+    ), lower(
+        substring(
+            name, 2, length(name)
+        )
+    )
+) as name
 from users
 order by user_id;
