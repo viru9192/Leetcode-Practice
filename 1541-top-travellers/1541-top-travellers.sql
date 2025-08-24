3f743b97caafd1c9b3d0289eebde84faba23e32b
Viranchi More <159642625+viru9192@users.noreply.github.com>
2025-08-24 16:19:19 -0400

    Time: 794 ms (73.01%), Space: 0B (100%) - LeetHub

--- 1541-top-travellers/1541-top-travellers.sql
+++ 1541-top-travellers/1541-top-travellers.sql
@@ -4,5 +4,5 @@ coalesce(sum(r.distance), 0) as travelled_distance
 from users u
 left join rides r
 on u.id = r.user_id
-group by u.id, u.name
+group by r.user_id, u.name
 order by travelled_distance desc, u.name asc;
