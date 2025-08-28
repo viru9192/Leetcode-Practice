dcbaecb4a1f452c35c87eb7dfb57c35f818cd4e4
Viranchi More <159642625+viru9192@users.noreply.github.com>
2025-08-27 23:24:30 -0400

    Time: 513 ms (39.3%), Space: 0B (100%) - LeetHub

--- 1245-user-activity-for-the-past-30-days-i/1245-user-activity-for-the-past-30-days-i.sql
+++ 1245-user-activity-for-the-past-30-days-i/1245-user-activity-for-the-past-30-days-i.sql
@@ -1,8 +1,7 @@
 select 
 activity_date as day,
 count(distinct user_id) as active_users
-from 
-activity
+from activity
 where 
 activity_date between '2019-06-28' and '2019-07-27'
 group by activity_date;
