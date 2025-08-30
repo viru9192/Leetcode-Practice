12988bd2c14b2ea35b5b3c9e0d4f609da3cbd71d
Viranchi More <159642625+viru9192@users.noreply.github.com>
2025-08-29 20:43:23 -0400

    Time: 1170 ms (81.13%), Space: 0B (100%) - LeetHub

--- 1153-product-sales-analysis-i/1153-product-sales-analysis-i.sql
+++ 1153-product-sales-analysis-i/1153-product-sales-analysis-i.sql
@@ -1,7 +1,8 @@
-select 
+select
 p.product_name,
 s.year,
-s.price 
-from sales s
+s.price
+from sales s 
 join product p
-on s.product_id = p.product_id;
+on s.product_id = p.product_id
+group by s.product_id, s.sale_id;
