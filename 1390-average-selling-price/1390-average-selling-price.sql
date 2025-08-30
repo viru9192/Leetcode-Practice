32e3ddc3ca7c6551db8e6939662fb07e6a3b6876
Viranchi More <159642625+viru9192@users.noreply.github.com>
2025-08-29 20:18:36 -0400

    Time: 975 ms (17.28%), Space: 0B (100%) - LeetHub

--- 1390-average-selling-price/1390-average-selling-price.sql
+++ 1390-average-selling-price/1390-average-selling-price.sql
@@ -1,10 +1,8 @@
 select 
 p.product_id as product_id,
-round (
-    ifnull (
-        sum(u.units * p.price) / sum(u.units)
-    , 0), 2
-) as average_price
+ROUND(
+  COALESCE( SUM(u.units * p.price) / NULLIF(SUM(u.units), 0), 0 )
+, 2) AS average_price
 from prices p
 left join unitssold u
 on p.product_id = u.product_id
