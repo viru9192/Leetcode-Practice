32cc13b1c30d5490c0f35fc0c54ae8e46002843b
Viranchi More <159642625+viru9192@users.noreply.github.com>
2025-09-03 11:43:08 -0400

    Time: 594 ms (39.7%), Space: 0B (100%) - LeetHub

--- 1135-customers-who-bought-all-products/1135-customers-who-bought-all-products.sql
+++ 1135-customers-who-bought-all-products/1135-customers-who-bought-all-products.sql
@@ -1,7 +1,10 @@
-select 
+select
 c.customer_id
 from customer c
 join product p
 on c.product_key = p.product_key
 group by c.customer_id
-having count(distinct c.product_key) = (select count(*) from product);
+having 
+count(distinct c.product_key) = (
+    select count(*) from product
+);
