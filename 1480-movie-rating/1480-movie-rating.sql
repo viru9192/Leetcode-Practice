7273a32d2dda0ae55b9588d6fbd17f515bff58d0
Viranchi More <159642625+viru9192@users.noreply.github.com>
2025-09-07 15:16:09 -0400

    Time: 1111 ms (90.68%), Space: 0B (100%) - LeetHub

--- 1480-movie-rating/1480-movie-rating.sql
+++ 1480-movie-rating/1480-movie-rating.sql
@@ -1,10 +1,10 @@
 select results from (
-    select 
+    select
     u.name as results
     from users u
     join movierating mr
     on u.user_id = mr.user_id
-    group by u.user_id, u.name
+    group by u.user_id
     order by count(*) desc, u.name asc
     limit 1
 ) as top_users
@@ -17,9 +17,9 @@ select results from (
     from movies m
     join movierating mr
     on m.movie_id = mr.movie_id
-    where year(created_at) = 2020 and month(created_at) = 02
-    group by m.movie_id, m.title
+    where 
+    year(created_at) = '2020' and month(created_at) = '02'
+    group by m.movie_id
     order by avg(mr.rating) desc, m.title asc
     limit 1
 ) as top_movies;
-
