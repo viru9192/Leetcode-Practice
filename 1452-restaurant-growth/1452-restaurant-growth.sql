8c6bb6ad8fdbb4fda730627f0f3ffcba2ab96da0
Viranchi More <159642625+viru9192@users.noreply.github.com>
2025-09-09 15:13:23 -0400

    Time: 329 ms (87.75%), Space: 0B (100%) - LeetHub

--- 1452-restaurant-growth/1452-restaurant-growth.sql
+++ 1452-restaurant-growth/1452-restaurant-growth.sql
@@ -1,23 +1,19 @@
-select
+select 
 visited_on,
-round(
-    sum(amount) over(
-        order by visited_on
-        rows between 6 preceding and current row
-    ), 2
+sum(amount) over(
+    rows between 6 preceding and current row
 ) as amount,
 round(
     avg(amount) over(
-        order by visited_on
         rows between 6 preceding and current row
     ), 2
 ) as average_amount
 from (
     select 
-    visited_on,
-    sum(amount) as amount
+    sum(amount) as amount,
+    visited_on
     from customer
     group by visited_on
-) as t
+) as daily_paid
 order by visited_on asc
 limit 100 offset 6;
