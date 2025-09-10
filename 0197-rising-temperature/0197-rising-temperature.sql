6ea62f716503d4d44d2e4462038af6ac75652d11
Viranchi More <159642625+viru9192@users.noreply.github.com>
2025-09-10 16:14:10 -0400

    Time: 466 ms (68.95%), Space: 0B (100%) - LeetHub

--- 0197-rising-temperature/0197-rising-temperature.sql
+++ 0197-rising-temperature/0197-rising-temperature.sql
@@ -1,9 +1,9 @@
 select 
-w1.id
+w2.id as Id
 from weather w1
 join weather w2
-on 
-datediff(
-    w1.recorddate, w2.recorddate
+on datediff(
+    w2.recorddate, w1.recorddate
 ) = 1
-and w1.temperature > w2.temperature;
+where 
+w2.temperature > w1.temperature;
