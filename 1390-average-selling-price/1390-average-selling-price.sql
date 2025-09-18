f6bf94ad517571212c3bd5005def427d3ccf8ec5
Viranchi More <159642625+viru9192@users.noreply.github.com>
2025-09-18 10:58:40 -0400

    Time: 904 ms (25.36%), Space: 0B (100%) - LeetHub

--- 1390-average-selling-price/1390-average-selling-price.sql
+++ 1390-average-selling-price/1390-average-selling-price.sql
@@ -2,7 +2,7 @@ select
 p.product_id,
 round(
     coalesce(
-        sum(p.price * u.units) / sum(u.units), 0
+        sum(p.price * u.units) / nullif(sum(u.units), 0), 0
 ), 2) as average_price
 from prices p
 left join unitssold u
