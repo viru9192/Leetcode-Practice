8d7e5ff42cd9bbc0d689d4de2d3cdf194bbd5c0d
Viranchi More <159642625+viru9192@users.noreply.github.com>
2025-09-18 10:57:09 -0400

    Time: 729 ms (83.22%), Space: 0B (100%) - LeetHub

--- 1390-average-selling-price/1390-average-selling-price.sql
+++ 1390-average-selling-price/1390-average-selling-price.sql
@@ -2,11 +2,11 @@ select
 p.product_id,
 round(
     coalesce(
-        sum(u.units * p.price) / nullif(sum(u.units), 0), 0
-    ), 2
-) as average_price
+        sum(p.price * u.units) / sum(u.units), 0
+), 2) as average_price
 from prices p
 left join unitssold u
 on p.product_id = u.product_id
-and u.purchase_date between p.start_date and p.end_date
+and 
+u.purchase_date between p.start_date and p.end_date
 group by p.product_id;
