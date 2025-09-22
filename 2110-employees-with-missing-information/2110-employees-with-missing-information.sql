542c9c7dd93d245c9e843fb705582d77086e287b
Viranchi More <159642625+viru9192@users.noreply.github.com>
2025-09-22 17:47:21 -0400

    Time: 590 ms (33.39%), Space: 0B (100%) - LeetHub

--- 2110-employees-with-missing-information/2110-employees-with-missing-information.sql
+++ 2110-employees-with-missing-information/2110-employees-with-missing-information.sql
@@ -1,4 +1,5 @@
-select e.employee_id 
+select 
+e.employee_id
 from employees e
 left join salaries s
 on e.employee_id = s.employee_id
@@ -6,9 +7,10 @@ where s.employee_id is null
 
 union
 
-select s.employee_id 
+select 
+s.employee_id
 from salaries s
 left join employees e
 on s.employee_id = e.employee_id
 where e.employee_id is null
-order by employee_id;
+order by employee_id asc;
