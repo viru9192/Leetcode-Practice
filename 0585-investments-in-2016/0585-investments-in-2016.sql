a0d6a73dd0f4182a0a1e1215521fedc44c99eb00
Viranchi More <159642625+viru9192@users.noreply.github.com>
2025-09-24 23:10:20 -0400

    Time: 564 ms (42.24%), Space: 0B (100%) - LeetHub

--- 0585-investments-in-2016/0585-investments-in-2016.sql
+++ 0585-investments-in-2016/0585-investments-in-2016.sql
@@ -6,17 +6,17 @@ from insurance
 where 
 tiv_2015 in (
     select 
-    tiv_2015 
+    tiv_2015
     from insurance
     group by tiv_2015
     having count(*) > 1
-)
-and 
-(lat,lon) in (
+    )
+and
+(lat, lon) in (
     select 
-    lat, 
+    lat,
     lon
     from insurance
-    group by lat, lon
+    group by lat,lon
     having count(*) = 1
-);
+    );
