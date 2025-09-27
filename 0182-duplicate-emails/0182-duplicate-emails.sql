017551974a0c3847ad9b06be5e0d2c389bc42d9c
Viranchi More <159642625+viru9192@users.noreply.github.com>
2025-09-26 20:13:15 -0400

    Time: 348 ms (77.28%), Space: 0B (100%) - LeetHub

--- 0182-duplicate-emails/0182-duplicate-emails.sql
+++ 0182-duplicate-emails/0182-duplicate-emails.sql
@@ -1,6 +1,5 @@
 select
-distinct p1.email as Email
-from person p1
-left join person p2
-on p1.id != p2.id 
-where p1.email = p2.email;
+email as Email
+from person
+group by email
+having count(*) > 1;
