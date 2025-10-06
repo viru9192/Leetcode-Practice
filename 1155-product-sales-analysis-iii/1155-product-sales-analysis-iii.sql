9691f1a0ab25154f19b1b4b558320cadf5560146
Viranchi More <159642625+viru9192@users.noreply.github.com>
2025-10-06 09:10:45 -0400

    Time: 1311 ms (5.17%), Space: 0B (100%) - LeetHub

--- 1155-product-sales-analysis-iii/1155-product-sales-analysis-iii.sql
+++ 1155-product-sales-analysis-iii/1155-product-sales-analysis-iii.sql
@@ -1,16 +1,15 @@
 select
-s.product_id, 
+s.product_id,
 s.year as first_year,
 s.quantity,
 s.price
 from sales s
 join (
-    select
-    sale_id,
+    select 
     product_id,
     min(year) as f_year
     from sales
     group by product_id
-) as fy
-on s.product_id = fy.product_id
-where s.year = fy.f_year;
+) as first_y
+on s.product_id = first_y.product_id
+where s.year = first_y.f_year;
