04c43c2313ae7670131903a2bf7cd216d78fe3e3
Viranchi More <159642625+viru9192@users.noreply.github.com>
2025-10-07 09:41:17 -0400

    Time: 923 ms (20.69%), Space: 0B (100%) - LeetHub

--- 1734-bank-account-summary-ii/1734-bank-account-summary-ii.sql
+++ 1734-bank-account-summary-ii/1734-bank-account-summary-ii.sql
@@ -3,6 +3,6 @@ u.name,
 sum(t.amount) as balance
 from users u
 join transactions t
-on u.account = t.account 
-group by t.account
-having balance > 10000;
+on u.account = t.account
+group by u.account
+having sum(t.amount) > 10000;
