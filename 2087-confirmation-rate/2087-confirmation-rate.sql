b0d0995f145942e20bcb14f2c113554c62f304b9
Viranchi More <159642625+viru9192@users.noreply.github.com>
2025-10-22 18:49:47 -0400

    Time: 619 ms (62.12%), Space: 0B (100%) - LeetHub

--- 2087-confirmation-rate/2087-confirmation-rate.sql
+++ 2087-confirmation-rate/2087-confirmation-rate.sql
@@ -2,7 +2,7 @@ select
 s.user_id,
 round(
     sum(
-        case when c.action = 'confirmed' then 1
+        case when c.action = 'confirmed' then 1 
         else 0 
         end
     ) / count(*), 2
