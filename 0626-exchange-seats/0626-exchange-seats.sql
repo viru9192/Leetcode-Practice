4aa9def0185f51ba19b3c52b0647433e3f895bce
Viranchi More <159642625+viru9192@users.noreply.github.com>
2025-10-26 13:38:27 -0400

    Time: 356 ms (30.3%), Space: 0B (100%) - LeetHub

--- 0626-exchange-seats/0626-exchange-seats.sql
+++ 0626-exchange-seats/0626-exchange-seats.sql
@@ -1,12 +1,10 @@
 select
-(
-    case 
-    when s1.id % 2 = 1 and s2.id is not null then s1.id + 1
-    when s1.id % 2 = 0 then s1.id - 1
-    else s1.id 
-    end
-) as id,
-s1.student 
+(case 
+when s1.id % 2 = 1 and s2.id is not null then s1.id + 1
+when s1.id % 2 = 0 then s1.id - 1
+else s1.id
+end) as id,
+s1.student
 from seat s1
 left join seat s2
 on s2.id = s1.id + 1
