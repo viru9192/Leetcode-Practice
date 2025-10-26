a0ba57d5010df61aa9ae6fb299bec1bfcb8c7cd2
Viranchi More <159642625+viru9192@users.noreply.github.com>
2025-10-25 20:36:57 -0400

    Time: 310 ms (85.85%), Space: 0B (100%) - LeetHub

--- 0570-managers-with-at-least-5-direct-reports/0570-managers-with-at-least-5-direct-reports.sql
+++ 0570-managers-with-at-least-5-direct-reports/0570-managers-with-at-least-5-direct-reports.sql
@@ -1,7 +1,5 @@
-select
-m.name
-from employee e
-join employee m
-on e.managerid = m.id
-group by e.managerid
-having count(*) >= 5;
+SELECT m.name
+FROM employee e
+JOIN employee m ON e.managerid = m.id
+GROUP BY m.id, m.name
+HAVING COUNT(*) >= 5;
