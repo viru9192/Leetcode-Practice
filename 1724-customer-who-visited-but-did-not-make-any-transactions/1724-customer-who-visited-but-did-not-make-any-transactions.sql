9a0bdc28b58c5229a4a4f5df1dbe7c438e0fb55d
Viranchi More <159642625+viru9192@users.noreply.github.com>
2025-10-30 09:42:28 -0400

    Time: 1250 ms (62.49%), Space: 0B (100%) - LeetHub

--- 1724-customer-who-visited-but-did-not-make-any-transactions/1724-customer-who-visited-but-did-not-make-any-transactions.sql
+++ 1724-customer-who-visited-but-did-not-make-any-transactions/1724-customer-who-visited-but-did-not-make-any-transactions.sql
@@ -1,8 +1,8 @@
 select
 v.customer_id,
-count(*) as count_no_trans
+count(v.visit_id) as count_no_trans
 from visits v
 left join transactions t
 on v.visit_id = t.visit_id
-where t.transaction_id is null
+where t.visit_id is null
 group by v.customer_id;
