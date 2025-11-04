d8e3fd1006b05eb548a5b18d47d99ad658fca0b2
Viranchi More <159642625+viru9192@users.noreply.github.com>
2025-11-04 00:21:20 -0500

    Time: 31 ms (33.77%), Space: 17.7 MB (83.64%) - LeetHub

--- 3610-find-x-sum-of-all-k-long-subarrays-i/3610-find-x-sum-of-all-k-long-subarrays-i.py
+++ 3610-find-x-sum-of-all-k-long-subarrays-i/3610-find-x-sum-of-all-k-long-subarrays-i.py
@@ -4,11 +4,9 @@ class Solution:
         ans = []
         for i in range(n - k + 1):
             window = nums[i:i+k]
-            cnt = Counter(window)  # value -> frequency in this window
+            cnt = Counter(window)  
 
-            # Sort by: higher frequency first, then higher value
             top = sorted(cnt.items(), key=lambda p: (-p[1], -p[0]))[:x]
 
-            # x-sum = keep only top x elements (by the rule), sum value * frequency
             ans.append(sum(val * freq for val, freq in top))
         return ans
