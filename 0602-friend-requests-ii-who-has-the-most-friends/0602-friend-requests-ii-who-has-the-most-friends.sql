2037bd31280e1ce1734fb59defca8ec84c096c6c
Viranchi More <159642625+viru9192@users.noreply.github.com>
2025-11-04 20:05:54 -0500

    Time: 298 ms (48.78%), Space: 0B (100%) - LeetHub

--- 0602-friend-requests-ii-who-has-the-most-friends/0602-friend-requests-ii-who-has-the-most-friends.sql
+++ 0602-friend-requests-ii-who-has-the-most-friends/0602-friend-requests-ii-who-has-the-most-friends.sql
@@ -1,16 +1,20 @@
 with counts as (
-    select 
+    select
     id,
     count(*) as num
     from (
-        select requester_id as id from RequestAccepted
+        select requester_id as id from requestaccepted
         union all
-        select accepter_id as id from RequestAccepted
-) as friends
+        select accepter_id as id from requestaccepted
+    ) as friends
 group by id
-) 
-select 
+)
+select
 id,
 num
 from counts
-where num = (select max(num) from counts);
+where num = (
+    select
+    max(num) as high
+    from counts
+);
