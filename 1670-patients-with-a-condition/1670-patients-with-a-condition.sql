c9ad0a43c67e2db8418beffedcc6496e3f7bbf5c
Viranchi More <159642625+viru9192@users.noreply.github.com>
2025-11-04 19:52:54 -0500

    Time: 438 ms (12.62%), Space: 0B (100%) - LeetHub

--- 1670-patients-with-a-condition/1670-patients-with-a-condition.sql
+++ 1670-patients-with-a-condition/1670-patients-with-a-condition.sql
@@ -1,6 +1,6 @@
-select *
-from 
-patients
-where 
-conditions like 'DIAB1%'
-or conditions like '% DIAB1%';
+select
+patient_id,
+patient_name,
+conditions
+from patients
+where conditions like 'DIAB1%' or conditions like '% DIAB1%';
