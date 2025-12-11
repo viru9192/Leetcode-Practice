63f58b1f7b3aad54c565967a8851145b52d46fc1
Viranchi More <159642625+viru9192@users.noreply.github.com>
2025-12-11 01:44:59 -0500

    Time: 1276 ms (66.13%), Space: 0B (100%) - LeetHub

--- 0607-sales-person/0607-sales-person.sql
+++ 0607-sales-person/0607-sales-person.sql
@@ -1,11 +1,11 @@
-select
-s.name
+select 
+s.name 
 from salesperson s
 where s.sales_id not in (
-    select
-    o.sales_id
+    select 
+    o.sales_id 
     from orders o
     join company c
     on o.com_id = c.com_id
-    where c.name = 'RED'
+    where c.name = "RED"
 );
