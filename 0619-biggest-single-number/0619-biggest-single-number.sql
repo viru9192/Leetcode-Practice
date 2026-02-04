f337a7ee259eeb762583a784fc8c455ab2094c77
Viranchi More <159642625+viru9192@users.noreply.github.com>
2026-02-04 18:25:23 -0500

    Time: 406 ms (58.99%), Space: 0B (100%) - LeetHub

--- 0619-biggest-single-number/0619-biggest-single-number.sql
+++ 0619-biggest-single-number/0619-biggest-single-number.sql
@@ -1,9 +1,9 @@
-select (
-    select
+select
+max(num) as num
+from (
+    select 
     num
-    from mynumbers 
+    from mynumbers
     group by num
     having count(*) = 1
-    order by num desc
-    limit 1
-) as num;
+) as un;
