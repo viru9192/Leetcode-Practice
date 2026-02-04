c7c2c2fc4542fa5fdefb37f483e82c3427c42a8a
Viranchi More <159642625+viru9192@users.noreply.github.com>
2026-02-04 18:46:48 -0500

    Time: 614 ms (54.62%), Space: 0B (100%) - LeetHub

---
+++ 1731-the-number-of-employees-which-report-to-each-employee/1731-the-number-of-employees-which-report-to-each-employee.sql
@@ -0,0 +1,12 @@
+select 
+m.employee_id,
+m.name,
+count(e.employee_id) as reports_count,
+round(
+    avg(e.age), 0
+) as average_age
+from employees e
+join employees m
+on e.reports_to = m.employee_id
+group by m.employee_id, m.name
+order by m.employee_id;
