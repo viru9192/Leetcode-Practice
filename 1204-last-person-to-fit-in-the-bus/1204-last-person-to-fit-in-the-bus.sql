0d8dbc06ae1cad1291ed6a62feb755f1fedd97eb
Viranchi More <159642625+viru9192@users.noreply.github.com>
2026-02-05 12:18:51 -0500

    Time: 1446 ms (22.22%), Space: 0B (100%) - LeetHub

---
+++ 1204-last-person-to-fit-in-the-bus/1204-last-person-to-fit-in-the-bus.sql
@@ -0,0 +1,13 @@
+select 
+person_name
+from (
+    select 
+    person_name,
+    sum(weight) over(
+        order by turn
+    ) as cumm_weight
+    from queue
+) cw
+where cumm_weight <= 1000
+order by cumm_weight desc
+limit 1;
