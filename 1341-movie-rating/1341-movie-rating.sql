fda3586e75d87f09f7c66020a70954591eecc549
Viranchi More <159642625+viru9192@users.noreply.github.com>
2026-02-06 11:29:37 -0500

    Time: 1112 ms (92.11%), Space: 0B (100%) - LeetHub

--- 1341-movie-rating/1341-movie-rating.sql
+++ 1341-movie-rating/1341-movie-rating.sql
@@ -4,7 +4,7 @@
     from users u
     join movierating mr
     on u.user_id = mr.user_id
-    group by u.name
+    group by u.user_id, u.name
     order by count(*) desc, u.name asc
     limit 1
 )
@@ -18,7 +18,7 @@ union all
     join movierating mr
     on m.movie_id = mr.movie_id
     and year(mr.created_at) = 2020 and month(mr.created_at) = 02
-    group by m.title
+    group by m.movie_id, m.title
     order by avg(mr.rating) desc, m.title asc
     limit 1
 );
