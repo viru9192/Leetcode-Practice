e2e805e70642c059e6a20d13577f7130997af673
Viranchi More <159642625+viru9192@users.noreply.github.com>
2026-02-28 23:19:02 -0500

    Time: 504 ms (91.36%), Space: 0B (100%) - LeetHub

--- 0550-game-play-analysis-iv/0550-game-play-analysis-iv.sql
+++ 0550-game-play-analysis-iv/0550-game-play-analysis-iv.sql
@@ -1,18 +1,18 @@
 select
 round(
     count(distinct a.player_id) / (
-        select 
-        count(distinct player_id) as cnt
+        select
+        count(distinct player_id) as player
         from activity
     ), 2
-) as fraction
+) as fraction 
 from activity a
 join (
     select
     player_id,
-    min(event_date) as flogin
+    min(event_date) as f_login
     from activity
     group by player_id
 ) as fl
 on a.player_id = fl.player_id
-and datediff(a.event_date, fl.flogin) = 1;
+and datediff(a.event_date, fl.f_login) = 1;
