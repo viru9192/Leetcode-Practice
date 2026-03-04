523a1658e6194c77eb3d77be5e98149f7a05b20c
Viranchi More <159642625+viru9192@users.noreply.github.com>
2026-03-04 18:45:27 -0500

    Time: 599 ms (41.5%), Space: 0B (100%) - LeetHub

--- 1193-monthly-transactions-i/1193-monthly-transactions-i.sql
+++ 1193-monthly-transactions-i/1193-monthly-transactions-i.sql
@@ -1,16 +1,18 @@
-select 
+select
 date_format(trans_date, '%Y-%m') as 'month',
 country,
-count(id) as trans_count,
+count(*) as trans_count,
 sum(
-    case when state = 'approved' then 1
-    else 0 
+    case
+        when state = 'approved' then 1
+        else 0
     end
 ) as approved_count,
 sum(amount) as trans_total_amount,
 sum(
-    case when state = 'approved' then amount
-    else 0
+    case
+        when state = 'approved' then amount
+        else 0
     end
 ) as approved_total_amount
 from transactions
