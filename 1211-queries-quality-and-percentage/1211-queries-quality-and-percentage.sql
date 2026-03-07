666045c4805a17ce1615c15cc19549ebaf1beb9f
Viranchi More <159642625+viru9192@users.noreply.github.com>
2026-03-07 11:10:25 -0500

    Time: 769 ms (5%), Space: 0B (100%) - LeetHub

--- 1211-queries-quality-and-percentage/1211-queries-quality-and-percentage.sql
+++ 1211-queries-quality-and-percentage/1211-queries-quality-and-percentage.sql
@@ -1,12 +1,13 @@
-select 
+select
 query_name,
 round(
-    sum(rating / position) / count(*), 2
+    avg(rating / position), 2
 ) as quality,
 round(
     sum(
-        case when rating < 3 then 1
-        else 0 
+        case 
+            when rating < 3 then 1
+            else 0
         end
     ) * 100 / count(*), 2
 ) as poor_query_percentage
