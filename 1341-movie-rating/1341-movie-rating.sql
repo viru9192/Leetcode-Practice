d7906b8928ccdb013dee61643bb257b6b94086b9
Viranchi More <159642625+viru9192@users.noreply.github.com>
2026-03-08 22:44:12 -0400

    Time: 1231 ms (66.16%), Space: 0B (100%) - LeetHub

--- 1341-movie-rating/1341-movie-rating.sql
+++ 1341-movie-rating/1341-movie-rating.sql
@@ -12,12 +12,12 @@
 union all
 
 (
-    select 
+    select
     m.title as results
     from movies m
     join movierating mr
     on m.movie_id = mr.movie_id
-    and year(mr.created_at) = 2020 and month(mr.created_at) = 02
+    where year(mr.created_at) = 2020 and month(mr.created_at) = 02
     group by m.movie_id, m.title
     order by avg(mr.rating) desc, m.title asc
     limit 1
