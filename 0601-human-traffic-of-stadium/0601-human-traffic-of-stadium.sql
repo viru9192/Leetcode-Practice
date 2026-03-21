0025ff63b5859fd83467ed419dbbb900352e73ca
Viranchi More <159642625+viru9192@users.noreply.github.com>
2026-03-21 19:43:44 -0400

    Time: 379 ms (35.47%), Space: 0B (100%) - LeetHub

--- 0601-human-traffic-of-stadium/0601-human-traffic-of-stadium.sql
+++ 0601-human-traffic-of-stadium/0601-human-traffic-of-stadium.sql
@@ -1,29 +1,29 @@
 with consec as (
-    select 
+    select
     id,
     visit_date,
     people,
     id - row_number() over(
-        order by id
+        order by id 
     ) as grp
-    from stadium
+    from stadium 
     where people >= 100
 ),
 consec_grp as (
-    select 
+    select
     grp 
     from consec
     group by grp
-    having count(*) >=3
+    having count(*) >= 3
 )
-select 
+select
 id,
 visit_date,
 people
 from consec
 where grp in (
-    select
+    select 
     grp 
     from consec_grp
-) 
+)
 order by visit_date;
