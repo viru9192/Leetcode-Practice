778dc0c07fa7c3070b05c7ec86db5875f9e63a87
Viranchi More <159642625+viru9192@users.noreply.github.com>
2026-03-22 11:05:38 -0400

    Time: 1035 ms (17.23%), Space: 0B (100%) - LeetHub

--- 0184-department-highest-salary/0184-department-highest-salary.sql
+++ 0184-department-highest-salary/0184-department-highest-salary.sql
@@ -5,7 +5,7 @@ e.salary as Salary
 from employee e
 join department d
 on e.departmentid = d.id
-where e.salary in (
+where e.salary = (
     select 
     max(e1.salary) as salary
     from employee e1
