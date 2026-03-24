05cf0d41a16dfedeb0bfbd133aeab1b9f7671ac7
Viranchi More <159642625+viru9192@users.noreply.github.com>
2026-03-24 17:05:20 -0400

    Time: 596 ms (21.99%), Space: 0B (100%) - LeetHub

--- 0262-trips-and-users/0262-trips-and-users.sql
+++ 0262-trips-and-users/0262-trips-and-users.sql
@@ -1,25 +1,25 @@
-with valid_s as (
+with can_s as(
     select
-    t.request_at,
-    t.status
+    t.status,
+    t.request_at
     from trips t
-    join users c 
+    join users c
     on t.client_id = c.users_id
-        and c.role = 'client' and c.banned = 'No'
+    and c.banned = 'No' and c.role = 'client'
     join users d
     on t.driver_id = d.users_id
-        and d.role = 'driver' and d.banned = 'No'
+    and d.banned = 'No' and d.role = 'driver'
+    where t.request_at between '2013-10-01' and '2013-10-03'
 )
-select
-v.request_at as 'Day',
+select 
+request_at as 'Day',
 round(
     sum(
-        case 
-            when v.status in ('cancelled_by_driver', 'cancelled_by_client') then 1
+        case
+            when status in ('cancelled_by_driver', 'cancelled_by_client') then 1
             else 0
         end
     ) / count(*), 2
 ) as 'Cancellation Rate'
-from valid_s v
-where v.request_at between '2013-10-01' and '2013-10-03'
-group by v.request_at;
+from can_s
+group by request_at;
