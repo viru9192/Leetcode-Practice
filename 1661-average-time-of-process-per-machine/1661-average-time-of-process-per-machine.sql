38e0ca7cff6ac00890a12ca736d6bd425fe58e94
Viranchi More <159642625+viru9192@users.noreply.github.com>
2026-04-05 19:33:26 -0400

    Time: 236 ms (65.64%), Space: 0B (100%) - LeetHub

--- 1661-average-time-of-process-per-machine/1661-average-time-of-process-per-machine.sql
+++ 1661-average-time-of-process-per-machine/1661-average-time-of-process-per-machine.sql
@@ -1,12 +1,10 @@
 select 
-a.machine_id,
+a1.machine_id,
 round(
-    avg(
-        b.timestamp - a.timestamp
-    ), 3
+    avg(a2.timestamp - a1.timestamp), 3
 ) as processing_time
-from activity a
-join activity b
-on a.machine_id = b.machine_id
-and a.activity_type = 'start' and b.activity_type = 'end'
-group by a.machine_id;
+from activity a1
+join activity a2
+on a1.machine_id = a2.machine_id
+and a1.activity_type = 'start' and a2.activity_type = 'end'
+group by a1.machine_id;
