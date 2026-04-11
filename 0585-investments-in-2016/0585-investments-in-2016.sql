f6713a786457f184330d906fde7f1b74c0ce59a5
Viranchi More <159642625+viru9192@users.noreply.github.com>
2026-04-11 13:20:34 -0400

    Time: 592 ms (29.55%), Space: 0B (100%) - LeetHub

--- 0585-investments-in-2016/0585-investments-in-2016.sql
+++ 0585-investments-in-2016/0585-investments-in-2016.sql
@@ -1,17 +1,19 @@
-select
+select 
 round(
-    sum(tiv_2016), 2
+    sum(
+        tiv_2016
+    ), 2
 ) as tiv_2016
 from insurance 
 where tiv_2015 in (
-    select
+    select 
     tiv_2015
     from insurance
     group by tiv_2015
     having count(*) > 1
-) and
+) and 
 (lat, lon) in (
-    select
+    select 
     lat,
     lon
     from insurance
