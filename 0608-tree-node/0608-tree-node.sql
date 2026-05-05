8fbe42f7ce0af367921a5ac8e3f05cda80cfbaa8
Viranchi More <159642625+viru9192@users.noreply.github.com>
2026-05-04 21:05:22 -0400

    Time: 467 ms (58.14%), Space: 0B (100%) - LeetHub

--- 0608-tree-node/0608-tree-node.sql
+++ 0608-tree-node/0608-tree-node.sql
@@ -1,15 +1,15 @@
 select 
-id,
+id, 
 (
     case
-        when p_id is null then "Root"
+        when p_id is null then 'Root'
         when p_id is not null and id in (
             select 
             p_id
             from tree
             where p_id is not null
-        ) then "Inner"
-        else "Leaf"
+        ) then 'Inner'
+        else 'Leaf'
     end
 ) as type
 from tree;
