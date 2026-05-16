8a0f51f707a1c86e6ee2aa774e3b3cd98ea2dd6d
Viranchi More <159642625+viru9192@users.noreply.github.com>
2026-05-16 12:28:46 -0400

    Time: 607 ms (24.7%), Space: 0B (100%) - LeetHub

--- 0262-trips-and-users/0262-trips-and-users.sql
+++ 0262-trips-and-users/0262-trips-and-users.sql
@@ -11,6 +11,7 @@ with users as (
     join users d
     on t.driver_id = d.users_id 
         and d.role = 'driver' and d.banned = 'No'
+    where request_at between '2013-10-01' and '2013-10-03'
 )
 select 
 request_at as 'Day',
@@ -23,5 +24,4 @@ round(
     ) * 1.0 / count(*), 2
 ) as 'Cancellation Rate'
 from users 
-where request_at between '2013-10-01' and '2013-10-03'
 group by request_at;
