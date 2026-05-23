b3f8137d92fca02bd75f276b14d7132f24575b1c
Viranchi More <159642625+viru9192@users.noreply.github.com>
2026-05-23 13:23:30 -0400

    Time: 600 ms (55.01%), Space: 0B (100%) - LeetHub

--- 1873-calculate-special-bonus/1873-calculate-special-bonus.sql
+++ 1873-calculate-special-bonus/1873-calculate-special-bonus.sql
@@ -1,9 +1,10 @@
 select
-e.employee_id,
-coalesce(e1.salary, 0) as bonus
-from employees e
-left join employees e1
-on e.employee_id = e1.employee_id
-and e.employee_id % 2 = 1
-and e.name not like 'M%'
-order by e.employee_id;
+employee_id,
+(
+    case
+        when employee_id % 2 = 1 and name not like 'M%' then salary
+        else 0
+    end
+) as bonus
+from employees
+order by employee_id;
