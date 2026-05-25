295b0bae461ba7b52eebd040b3883db076254686
Viranchi More <159642625+viru9192@users.noreply.github.com>
2026-05-24 20:07:53 -0400

    Time: 558 ms (36.96%), Space: 0B (100%) - LeetHub

--- 1795-rearrange-products-table/1795-rearrange-products-table.sql
+++ 1795-rearrange-products-table/1795-rearrange-products-table.sql
@@ -1,24 +1,24 @@
-select
+select 
 product_id,
 'store1' as store,
-store1 as price 
+store1 as price
 from products
 where store1 is not null
 
 union all
 
-select
+select 
 product_id,
 'store2' as store,
-store2 as price 
+store2 as price
 from products
 where store2 is not null
 
 union all
 
-select
+select 
 product_id,
 'store3' as store,
-store3 as price 
+store3 as price
 from products
 where store3 is not null;
