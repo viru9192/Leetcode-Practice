647fed58d614153d1b08f547e9389b3ec05aa428
Viranchi More <159642625+viru9192@users.noreply.github.com>
2026-08-09 23:34:29 -0400

    Time: 648 ms (30.21%), Space: 0B (100%) - LeetHub

--- 1789-primary-department-for-each-employee/1789-primary-department-for-each-employee.sql
+++ 1789-primary-department-for-each-employee/1789-primary-department-for-each-employee.sql
@@ -1,13 +1,13 @@
 select 
-employee_id, 
+employee_id,
 department_id
 from employee
 where primary_flag = 'Y'
 
-union all
+union all 
 
 select 
-employee_id, 
+employee_id,
 department_id
 from employee
 group by employee_id
