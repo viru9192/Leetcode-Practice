df0300fa2b650692baa33bac0e0347b7e414726d
Viranchi More <159642625+viru9192@users.noreply.github.com>
2026-08-10 22:48:26 -0400

    Time: 657 ms (81.71%), Space: 0B (100%) - LeetHub

--- 1934-confirmation-rate/1934-confirmation-rate.sql
+++ 1934-confirmation-rate/1934-confirmation-rate.sql
@@ -1,14 +1,20 @@
+with cte as (
+    select 
+    user_id,
+    round(
+        count(
+                case
+                    when action = 'confirmed' then 1
+                end
+            ) * 1.0 / count(*), 2
+        ) as confirmation_rate
+    from confirmations
+    group by user_id
+)
 select 
 s.user_id,
-round(
-    coalesce(
-            count(
-                case
-                    when c.action = 'confirmed' then 1
-                end) * 1.0 / count(*), 0
-            ), 2
-) as confirmation_rate
+coalesce(c.confirmation_rate, 0) as confirmation_rate
 from signups s
-left join confirmations c
+left join cte c
 on s.user_id = c.user_id
 group by s.user_id;
